2bbc6dfede460794f980a0502d1f3c592528972d
Nana <nanaolomaiyani@gmail.com>
2022-11-28 23:11:05 +0300

    commit tree fb25db10c173e6b2896a17351ab1a9823ad6d1d0

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,17 +4,17 @@ class AuthorsController < ApplicationController
   def index
     authors = Author.all 
     render json: authors
+    render json: authors, include:['posts', 'posts.tags', 'profile']
   end
 
   def show
     author = Author.find(params[:id])
     render json: author
+    render json: author, include:['posts', 'posts.tags', 'profile']
   end
 
   private
-
   def render_not_found_response
     render json: { error: "Author not found" }, status: :not_found
   end
-
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,19 @@
 class PostsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
+  
   def index
     posts = Post.all
     render json: posts
   end
-
   def show
     post = Post.find(params[:id])
     render json: post
   end
 
+  def tags
+    post = Post.find_by(params[:id])
+    render json: post.tags
+  end
   private
 
   def render_not_found_response
